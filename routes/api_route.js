6716361d0d4dd1bdf2d01895ac48f65e5e8da4f2
Jeffrey Bahns <jeffbahns@gmail.com>
2017-04-17 15:57:44 -0700

    added some api endpoints

--- routes/api_route.js
+++ routes/api_route.js
@@ -5,22 +5,31 @@ var gameDal = require('../model/game_dal');
 var playerDal = require('../model/player_dal');
 
 
-router.get('/', function(req, res) {
+router.get('/all_games', function(req, res) {
     gameDal.GetAll(function(err, result) {
         if(err) {
             res.send(err);
         }
         res.render('api/endpoint.ejs', {rs: result});
     });
-    //res.render('api/endpoint.ejs');
 });
 
-router.get('/', function(req, res) {
-    res.render('api/endpoint.ejs');
+router.get('/all_courts', function(req, res) {
+    courtDal.GetAll(function(err, result) {
+        if(err) {
+            res.send(err);
+        }
+        res.render('api/endpoint.ejs', {rs: result});
+    });
 });
 
-router.get('/', function(req, res) {
-    res.render('api/endpoint.ejs');
+router.get('/all_players', function(req, res) {
+    playerDal.GetAll(function(err, result) {
+        if(err) {
+            res.send(err);
+        }
+        res.render('api/endpoint.ejs', {rs: result});
+    });
 });
 
 module.exports = router;
